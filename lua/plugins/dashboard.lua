79884faaf7e14df179ddc02843ae5e2727d8abae
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2024-07-10 15:13:34 -0400

    fix: disable retrail when open dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -5,6 +5,7 @@ return {
     return require "configs.dashboard"
   end,
   config = function(_, opts)
+    vim.cmd "RetrailDisable"
     require("dashboard").setup(opts)
   end,
   dependencies = { { "nvim-tree/nvim-web-devicons" } },
