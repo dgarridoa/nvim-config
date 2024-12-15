50068f7b0f4f09ce19c68c44a0254a247ab91d41
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2024-12-14 22:35:31 -0300

    feat: add live_multigrep to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,53 @@
+local live_multigrep = function(opts)
+  local pickers = require "telescope.pickers"
+  local finders = require "telescope.finders"
+  local make_entry = require "telescope.make_entry"
+  local conf = require("telescope.config").values
+
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.fn.getcwd()
+
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      return vim
+        .iter({
+          args,
+          { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+        })
+        :flatten()
+        :totable()
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = "Multi Grep",
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require("telescope.sorters").empty(),
+    })
+    :find()
+end
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -15,7 +65,7 @@ return {
         desc = "Open file browser",
       },
       { "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<cr>", desc = "Find all" },
-      { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>fw", live_multigrep, desc = "Live grep" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help page" },
       { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Find oldfiles" },
