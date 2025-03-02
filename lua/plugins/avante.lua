f29b1b2c73db96d1732289db5e81accceb376b97
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2025-03-01 22:16:45 -0300

    feat: use o3-mini in copilot provider in avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -7,13 +7,10 @@ return {
     provider = "copilot",
     auto_suggestion_provider = "copilot",
     copilot = {
-      model = "gpt-4o",
-      frequency_penalty = 0,
-      presence_penalty = 0,
-      max_tokens = 2000,
+      model = "o3-mini",
+      timeout = 30000,
       temperature = 0,
-      top_p = 1,
-      n = 1,
+      max_tokens = 4096,
     },
   },
   build = "make", -- run :AvanteBuild if failed to load avante_repo_map
